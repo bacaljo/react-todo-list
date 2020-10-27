d7a38263363ac8c7f6054948f9492bf98c27cb4b
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-27 21:33:18 +0800

    [ReactReduxTodoList][John-Vance][Refactor]: Change to do group from class to a function component

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,14 +1,11 @@
-import React, {Component} from 'react'
 import TodoItemContainer from '../containers/TodoItemContainer'
 
-class TodoGroup extends Component {
-    render() {
-        return (
-            <ul className="todo-group">
-                {this.props.todoList.map(todo => <TodoItemContainer key={todo.id} id={todo.id} text={todo.text} />)}
-            </ul>
-        )
-    }
+function TodoGroup(props) {
+    return (
+        <ul className="todo-group">
+            {props.todoList.map(todo => <TodoItemContainer key={todo.id} id={todo.id} text={todo.text} />)}
+        </ul>
+    )
 }
 
 export default TodoGroup
