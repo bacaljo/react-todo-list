7a8c9965747481c54118507932532a3749465018
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-27 21:37:39 +0800

    [ReactReduxTodoList][John-Vance][Design]: Add design to todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,7 @@ import TodoGeneratorContainer from '../containers/TodoGeneratorContainer'
 function TodoList() {
     return (
         <div className="todo-list">
+            <h1>TodoList</h1>
             <TodoGroupContainer />
             <TodoGeneratorContainer />
         </div>
