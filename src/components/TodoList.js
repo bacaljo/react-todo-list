c6333f70b95b70e108b42237fd7a64bf777e4203
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-27 21:28:55 +0800

    [React-TodoList][John-Vance][Feature]: Enhance design

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,7 @@ import TodoGeneratorContainer from '../containers/TodoGeneratorContainer'
 function TodoList() {
     return (
         <div className="todo-list">
+            <h1>Todo List</h1>
             <TodoGroupContainer />
             <TodoGeneratorContainer />
         </div>
