04baf1a83280c9400aa319d630134ab7b1c11155
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-27 21:33:33 +0800

    [ReactReduxTodoList][John-Vance][Refactor]: Extract action type DELETE_TODO_ITEM to a constant

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,7 @@
+export const DELETE_TODO_ITEM = 'DELETE_TODO_ITEM'
+
 export const deleteTodoItem = (id) => {
     return {
-        type: 'DELETE_TODO_ITEM', payload: id
+        type: DELETE_TODO_ITEM, payload: id
     }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux'
+import {DELETE_TODO_ITEM} from './actions'
 
 const todoList = (state = [], action) => {
-    if (action.type === 'DELETE_TODO_ITEM') {
+    if (action.type === DELETE_TODO_ITEM) {
         const todoItem = state.find(todoItem => todoItem.id === action.payload)
         const index = state.indexOf(todoItem)
 
