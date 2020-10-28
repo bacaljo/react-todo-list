6a73a158a5e7fb692182c34f3213ef04f612883f
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 16:06:54 +0800

    [ReactReduxTodoList][John][Refactor]: Make action declarations one-liner

--- src/actions.js
+++ src/actions.js
@@ -2,10 +2,6 @@ export const DELETE_TODO_ITEM = 'DELETE_TODO_ITEM'
 export const ADD_TODO_ITEM = 'ADD_TODO_ITEM'
 export const TOGGLE_TODO_ITEM_STATE = 'TOGGLE_TODO_ITEM_STATE'
 
-export const deleteTodoItem = (id) => ({type: DELETE_TODO_ITEM, payload: id})
-
-export const addTodoItem = (todo) => 
-    ({type: ADD_TODO_ITEM, payload: todo})
-
-export const toggleTodoItemState = (id) => 
-    ({type: TOGGLE_TODO_ITEM_STATE, payload: id})
+export const deleteTodoItem = (id) => ({ type: DELETE_TODO_ITEM, payload: id })
+export const addTodoItem = (todo) => ({ type: ADD_TODO_ITEM, payload: todo })
+export const toggleTodoItemState = (id) => ({ type: TOGGLE_TODO_ITEM_STATE, payload: id })
