4891f9e6c53241bf172021aebdddf6ee8e531514
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 10:46:00 +0800

    [ReactReduxTodoList][John-Vance][Refactor]: Use stronger comparator

--- src/reducers.js
+++ src/reducers.js
@@ -4,7 +4,7 @@ import {ADD_TODO_ITEM, DELETE_TODO_ITEM, TOGGLE_TODO_ITEM_STATE} from './actions
 const todoList = (state = [], action) => {
     switch(action.type) {
         case DELETE_TODO_ITEM:
-            return state.filter(todoItem => todoItem.id != action.payload)
+            return state.filter(todoItem => todoItem.id !== action.payload)
         case ADD_TODO_ITEM:
             return [...state, action.payload]
         case TOGGLE_TODO_ITEM_STATE:
