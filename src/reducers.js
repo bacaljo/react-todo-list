9a98ce3b4f35f8aa0841b586a1b90ff057f86aca
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 10:19:48 +0800

    [ReactReduxTodoList][John-Vance][Refactor]: Change if-else to switch-case in reducer

--- src/reducers.js
+++ src/reducers.js
@@ -2,17 +2,18 @@ import {combineReducers} from 'redux'
 import {ADD_TODO_ITEM, DELETE_TODO_ITEM, TOGGLE_TODO_ITEM_STATE} from './actions'
 
 const todoList = (state = [], action) => {
-    if (action.type === DELETE_TODO_ITEM) {
-        return state.filter(todoItem => todoItem.id != action.payload)
-    } else if (action.type === ADD_TODO_ITEM) {
-        return [...state, action.payload]
-    } else if (action.type === TOGGLE_TODO_ITEM_STATE) {
-        return state.map(todoItem => (todoItem.id === action.payload)
-            ? {...todoItem, done: !todoItem.done}
-            : todoItem)
+    switch(action.type) {
+        case DELETE_TODO_ITEM:
+            return state.filter(todoItem => todoItem.id != action.payload)
+        case ADD_TODO_ITEM:
+            return [...state, action.payload]
+        case TOGGLE_TODO_ITEM_STATE:
+            return state.map(todoItem => (todoItem.id === action.payload)
+                ? {...todoItem, done: !todoItem.done}
+                : todoItem)
+        default:
+            return state
     }
-
-    return state
 }
 
 export default combineReducers({todoList})
